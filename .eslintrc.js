dc9d6f7e768ba1f38098df989e08909b99e6a753
Wandi <wandi.wandi@salt.co.id>
2021-10-08 13:07:35 +0700

    chore: update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,8 @@ const WRN = 1 // Turn the rule on as a warning (doesn't affect exit code)
 const ERR = 2 // Turn the rule on as an error (exit code will be 1)
 
 module.exports = {
-  root: true,
   env: {
-    'es6': true
+    'es6': true,
   },
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
@@ -14,22 +13,38 @@ module.exports = {
   ],
   plugins: [],
   reportUnusedDisableDirectives: true,
+  root: true,
   rules: {
-    'prettier/prettier': OFF,
+    '@typescript-eslint/explicit-function-return-type': OFF,
     '@typescript-eslint/explicit-module-boundary-types': OFF,
+    '@typescript-eslint/member-delimiter-style': OFF,
     '@typescript-eslint/no-empty-function': OFF,
+    '@typescript-eslint/no-var-requires': OFF,
+    '@typescript-eslint/type-annotation-spacing': ERR,
+    'array-bracket-spacing': [ERR, 'never'],
     'array-element-newline': [ERR, 'consistent'],
     'arrow-parens': [ERR, 'always'],
-    'comma-dangle': [ERR, 'never'],
-    'comma-spacing': [ERR, { 'before': false, 'after': true }],
+    'comma-dangle': [ERR, {'functions': 'never', 'objects': 'always-multiline'}],
+    'comma-spacing': [ERR, {'after': true, 'before': false}],
+    'curly': [ERR, 'multi'],
     'eol-last': [WRN, 'always'],
     'eqeqeq': WRN,
-    'indent': [ERR, 2, { 'SwitchCase': 1 }],
+    'indent': [ERR, 2, {'SwitchCase': 1}],
+    'key-spacing': [ERR, {'beforeColon': false}],
     'no-alert': OFF,
-    'no-console': WRN,
+    'no-console': OFF,
     'no-multi-spaces': [ERR],
-    'object-curly-spacing': [ERR, 'always'],
+    'no-multiple-empty-lines': [ERR, {'max': 2}],
+    'object-curly-spacing': [ERR, 'never', {'arraysInObjects': false}],
+    'prettier/prettier': OFF,
     'quotes': [ERR, 'single'],
-    'semi': [ERR, 'never']
-  }
+    'react/react-in-jsx-scope': OFF,
+    'semi': [ERR, 'never'],
+    'sort-imports': [OFF],
+    'sort-keys': [WRN, 'asc', {'minKeys': 3}],
+    'sort-vars': [WRN],
+    'space-before-blocks': [ERR, 'always'],
+    'space-before-function-paren': [ERR, 'never'],
+    'space-in-parens': [ERR, 'never'],
+  },
 }
